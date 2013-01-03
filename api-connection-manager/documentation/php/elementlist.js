a696d976c5ae3687989bda03b641ca765843e724
daithi <daithi@daithi-laptop>
2013-01-03 15:12:41 +0000

    fix directories

--- api-connection-manager/documentation/php/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","API_Connection_Manager"],["f","api_connection_manager_dash()"],["c","API_Connection_Manager_Setup"],["f","ar_print()"],["f","ar_to_string()"],["f","debug_print()"],["f","log_file()"],["f","rand_md5()"],["f","url_query_append()"]];
