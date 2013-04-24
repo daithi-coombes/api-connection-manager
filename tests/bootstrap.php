5eee37df222333fbeea68ae8fb49272b5f3472f7
daithi-coombes <webeire@gmail.com>
2013-04-24 21:51:06 +0100

    integrate wp test cases

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,13 @@
 <?php
 
-require_once('../class-api-connection-manager.php');
-require_once('../vendor/log4php/Logger.php');
+//change this to your path
+$path = '/var/www/wordpress.loc/wordpress-tests/bootstrap.php';
+
+if (file_exists($path)) {
+        $GLOBALS['wp_tests_options'] = array(
+                'active_plugins' => array('api-connection-manager/index.php')
+        );
+        require_once $path;
+} else {
+        exit("Couldn't find wordpress-tests/bootstrap.phpn");
+}
