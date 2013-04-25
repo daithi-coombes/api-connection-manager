199e2987e2a4056b52876874a5352af8d68933c6
daithi-coombes <webeire@gmail.com>
2013-04-25 14:19:55 +0100

    use WP bootstrap to set wp_options

---
+++ tests/config.php
@@ -0,0 +1,13 @@
+<?php
+$services = array(
+	'google/index.php' => array(
+		'app-grant-vars' => array(
+			'client_id' => '',
+			'redirect_uri' => '',
+			'scope' => 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
+		),
+		'app-token-vars' => array(
+			'client_secret' => ''
+		)
+	)
+)
