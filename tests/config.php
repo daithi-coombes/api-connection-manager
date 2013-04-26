53463059fc2d7770e5645257155198ea06ba8e45
daithi-coombes <webeire@gmail.com>
2013-04-26 16:46:33 +0100

    init commit

--- tests/config.php
+++ tests/config.php
@@ -2,12 +2,12 @@
 $services = array(
 	'google/index.php' => array(
 		'app-grant-vars' => array(
-			'client_id' => '',
-			'redirect_uri' => '',
+			'client_id' => 'foo',
+			'redirect_uri' => 'bar',
 			'scope' => 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
 		),
 		'app-token-vars' => array(
-			'client_secret' => ''
+			'client_secret' => 'baz'
 		)
 	)
 )
