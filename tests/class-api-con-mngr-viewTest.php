7e68122bf88f6fa19301f988d100f4d7985c3bb8
daithi-coombes <webeire@gmail.com>
2013-05-04 23:27:35 +0100

    view class unit tests

--- tests/class-api-con-mngr-viewTest.php
+++ tests/class-api-con-mngr-viewTest.php
@@ -1,6 +1,37 @@
 <?php
 
 class API_Con_Mngr_ViewTest extends WP_UnitTestCase{
-	
+
+	protected $view;
+
+	function setUp(){
+		parent::setUp();
+
+		if(!defined("DOING_AJAX"))
+			define('DOING_AJAX', true);
+
+		//load view class
+		$this->slug = get_option('test_slug');
+		require("../api-con-mngr-modules/{$this->slug}");
+		$this->view = new API_Con_Mngr_View();
+
+		//sign in user
+		$this->user = wp_signon(array(
+			'user_login' => 'admin',
+			'user_password' => 'password'));
+		wp_set_current_user($this->user->ID);
+	}
+
+	function test_get_head(){
+		$this->assertInternalType('string', $this->view->get_head());
+	}
+
+	function test_get_footer(){
+		$this->assertInternalType('string', $this->view->get_footer());
+	}
+
+	function test_get_html(){
+		$this->assertInternalType('string', $this->view->get_html(false));
+	}
 }
 
