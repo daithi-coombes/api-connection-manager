0336f0e4191f3f6f7f14190bb9e28a652979f9ff
daithi-coombes <webeire@gmail.com>
2013-05-09 10:33:16 +0100

    init commit

--- tests/class-api-connection-manager-userTest.php
+++ tests/class-api-connection-manager-userTest.php
@@ -2,5 +2,22 @@
 
 class API_Connection_Manager_UserTest extends WP_UnitTestCase{
 	
+	protected $api;
+	protected $user;
+	protected $slug;
+
+	function setUp(){
+		parent::setUp();
+
+		$this->api = new API_Connection_Manager();
+		$this->slug = get_option('test_slug');
+		$this->user = new API_Connection_Manager_User();
+		$_SESSION['Api-Con-Errors'] = array("test error");
+	}
+
+	function test_connect_user(){
+		$module = $this->api->get_service($this->slug);
+		ar_print($module);
+	}
 	
 }
