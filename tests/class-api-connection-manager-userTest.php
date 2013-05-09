f0c617da2ab8e7dcb42cc3f5a5affab5d607c137
daithi-coombes <webeire@gmail.com>
2013-05-09 11:01:28 +0100

    fixed

--- tests/class-api-connection-manager-userTest.php
+++ tests/class-api-connection-manager-userTest.php
@@ -4,6 +4,7 @@ class API_Connection_Manager_UserTest extends WP_UnitTestCase{
 	
 	protected $api;
 	protected $user;
+	protected $module;
 	protected $slug;
 
 	function setUp(){
@@ -17,7 +18,30 @@ class API_Connection_Manager_UserTest extends WP_UnitTestCase{
 
 	function test_connect_user(){
 		$module = $this->api->get_service($this->slug);
-		ar_print($module);
+		ar_print($module->get_connections());
+		$GLOBALS['wp_tests_options']['API_Con_Mngr_Module-connections'] = array();
+		$this->user->disconnect();
+		$uid = "1234";
+		$login = $module->login($uid);
+		
+		$this->assertEquals('1', $login);
+		$GLOBALS['wp_tests_options']['API_Con_Mngr_Module-connections'] = array(
+			$this->slug => array(1,123456)
+		);
+	}
+
+	function test_disconnect_user(){
+
+		$module = $this->api->get_service($this->slug);
+		$GLOBALS['wp_tests_options']['API_Con_Mngr_Module-connections'] = array(
+			$this->slug => array()
+		);
+		$this->user->disconnect();
+
+		$this->assertEquals($module->get_connections()[$this->slug], array());
+		$GLOBALS['wp_tests_options']['API_Con_Mngr_Module-connections'] = array(
+			$this->slug => array(1,123456)
+		);
 	}
 	
 }
