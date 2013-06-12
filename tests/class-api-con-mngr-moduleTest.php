8a98daa2e869a03a0fafb59d418bc9c568128c4e
daithi-coombes <webeire@gmail.com>
2013-06-12 10:19:42 +0100

    jenkins: use new workspace

--- tests/class-api-con-mngr-moduleTest.php
+++ tests/class-api-con-mngr-moduleTest.php
@@ -20,7 +20,7 @@ class API_Con_Mngr_ModuleTest extends WP_UnitTestCase{
 		//sign in user
 		$this->user = wp_signon(array(
 			'user_login' => 'admin',
-			'user_password' => 'a'));
+			'user_password' => 'password'));
 		$this->module->user = $this->user;
 		wp_set_current_user($this->user->ID);
 	}
